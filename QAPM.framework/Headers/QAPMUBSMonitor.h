da98a51e0587dc41267212c7b1a6d5525d5aeb4b
wxyawang <v_wxyawang@tencent.com>
2024-06-11 17:03:34 +0800

    update:更新SDK V5.3.7

--- QAPM.framework/Headers/QAPMUBSMonitor.h
+++ QAPM.framework/Headers/QAPMUBSMonitor.h
@@ -49,8 +49,18 @@ NS_ASSUME_NONNULL_BEGIN
                          action:(NSString *)action
                           value:(NSNumber *)value
                            tags:(NSDictionary<NSString *, NSString *> *)tags
-                         values:(NSDictionary<NSString *, NSNumber *> *)values;
+                         values:(NSDictionary<NSString *, NSNumber *> *)values __attribute__((deprecated("The method has been deprecated")));
 
+/**
+ 用户自定义用户行为操作调用,外部用户接口，调用该接口时请完成QAPM的一系列初始化操作，设置完QAPM的appKey后调用。
+ @param category  事件名，建议全部用大写字母表示
+ @param tags      字符串的map标记，对应的key的值只能是d1~d30/info1~info10范围的值
+ @param values    数值的map标记，对应的key的值只能是v1~v30范围的值
+ @return 用户行为事件 uuid，如果返回为nil，则表示生成自定义用户行为事件失败
+*/
+- (NSString *)customEvent:(NSString *)category
+                     tags:(NSDictionary<NSString *, NSString *> *)tags
+                   values:(NSDictionary<NSString *, NSNumber *> *)values;
 
 /**
  添加分桶实验(例如A/B test)，可添加多个，多次调用即可.
